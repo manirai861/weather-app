bec21168aeac95ad9d9261822f2d4419a4c60809
Shivam Mishra <mshivam019@gmail.com>
2023-06-18 00:57:37 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   const handleSearch = async () => {
     setIsLoading(true);
-    const apiKey = process.env.VITE_OPEN_WEATHER_MAP_API_KEY;
+    const apiKey = import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${apiKey}&units=metric`;
     try {
       await fetch(apiUrl);
